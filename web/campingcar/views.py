478c4a0ba4f7d71e463dafbe72d36030e7d559ba
rubinstory@naver.com <rubinstory@naver.com>
2020-12-11 14:56:04 +0900

    견적 페이지 추가

--- web/campingcar/views.py
+++ web/campingcar/views.py
@@ -7,3 +7,7 @@ def main(request, carType = 0):
 	if carType != 0:
 		carList = Car.objects.filter(type = carType)
 	return render(request, 'campingcar/index.html', {'carList' : carList})
+
+def estimate(request, name = 0):
+	car = Car.objects.get(name = name)
+	return render(request, 'campingcar/estimate.html', {'car' : car})
