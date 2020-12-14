686265e2e60778f55a40eee172bedcdabfe4defb
rubinstory@naver.com <rubinstory@naver.com>
2020-12-14 14:54:09 +0900

    딕셔너리 자료형으로 출력

--- web/campingcar/views.py
+++ web/campingcar/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render, redirect
 from .models import Car
 
-
 def main(request, carType = 0):
 	carList = Car.objects.all()
 	if carType != 0:
@@ -14,5 +13,12 @@ def detail(request):
 def estimate(request, name = 0):
 	car = Car.objects.get(name = name)
 	if request.method == "POST":
-		option_dict = request.POST
+		option_dict = {}
+		option_dict['car'] = car
+		for option in option_dict.keys():
+			if 'csrf' not in option:
+				option_dict[option] = request.POST[option]
+		for item in option_dict.keys():
+			print("%s, %s"%(item, option_dict[item]))
+
 	return render(request, 'campingcar/estimate.html', {'car' : car})
